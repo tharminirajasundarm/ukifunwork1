7bac18744d182a91db3ff3443abc64077b8f6c3d
tharminirajasundarm <tharmini9615@gmail.com>
2019-06-28 19:45:58 +0530

    funwork

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 $(document).ready(function() { SC.stream('/tracks/36796353',function(sound){
-    $('#start').click(function(e) {
+    $('#start1').click(function(e) {
            e.preventDefault();
            sound.start();
          });
-         $('#stop').click(function(e) {
+         $('#stop1').click(function(e) {
                     e.preventDefault();
                     sound.stop();
                   });
