4c16568b4450be62d28e0cc9e842bbd625def2e2
tharminirajasundarm <tharmini9615@gmail.com>
2019-07-02 10:28:54 +0530

    fun work

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ SC.initialize({
   client_id: '340f063c670272fac27cfa67bffcafc4'
 });
 $(document).ready(function() { SC.stream('tracks/637394484',function(sound){
-<<<<<<< HEAD
-    $('#start').click(function(e) {
-=======
+
     $('#start1').click(function(e) {
            e.preventDefault();
            sound.start();
