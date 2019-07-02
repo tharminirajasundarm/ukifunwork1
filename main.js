1e2de1979596d77a927a6b15a661b3b12706e835
tharminirajasundarm <tharmini9615@gmail.com>
2019-07-02 13:34:21 +0530

    fun work

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+
+
 SC.initialize({
-  client_id: '340f063c670272fac27cfa67bffcafc4'
+client_id:'95f22ed54a5c297b1c41f72d713623ef'
+
 });
-$(document).ready(function() { SC.stream('tracks/637394484',function(sound){
 
-    $('#start1').click(function(e) {
-           e.preventDefault();
-           sound.start();
-         });
-         $('#stop1').click(function(e) {
-                    e.preventDefault();
-                    sound.stop();
-                  });
+$(document).ready(function()
+{ SC.stream('/tracks/637394484',function(sound){
+  $('#start').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
 
-  });
+    $('#stop').click(function(e) {
+                   e.preventDefault();
+                   sound.stop();
+                 });
+});
 });
