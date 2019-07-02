9939b84256047f3640c9b760aae44cf801de3b36
tharminirajasundarm <tharmini9615@gmail.com>
2019-07-02 13:40:01 +0530

    fun work

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ client_id:'95f22ed54a5c297b1c41f72d713623ef'
 });
 
 $(document).ready(function()
-{ SC.stream('/tracks/637394484',function(sound){
+{ SC.stream('/tracks/580908972',function(sound){
   $('#start').click(function(e) {
           e.preventDefault();
           sound.start();
@@ -18,3 +18,5 @@ $(document).ready(function()
                  });
 });
 });
+
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/580908972&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
